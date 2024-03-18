469951a0a57547e94d3adc8b8e0f7c06c37f6524
Oleksii Oliinyk <oliynik.alosha@gmail.com>
2024-03-18 21:36:33 +0100

    Added new folder "ochrona-zasobów" with the same calculator and new logging mechanic.

--- ochrona-zasobów/app/calc.php
+++ ochrona-zasobów/app/calc.php
@@ -11,91 +11,161 @@ require_once dirname(__FILE__).'/../config.php';
 include _ROOT_PATH.'/app/security/check.php';
 
 //pobranie parametrów
-function getParams(&$x,&$y,&$operation){
-	$x = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
-	$y = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
-	$operation = isset($_REQUEST['op']) ? $_REQUEST['op'] : null;	
+function getParams(&$loan_am,&$show_rate,&$term){
+	$loan_am = isset($_REQUEST['loan_am']) ? $_REQUEST['loan_am'] : null;
+	$show_rate = isset($_REQUEST['rate']) ? $_REQUEST['rate'] : null;
+	$term = isset($_REQUEST['term']) ? $_REQUEST['term'] : null;	
 }
 
 //walidacja parametrów z przygotowaniem zmiennych dla widoku
-function validate(&$x,&$y,&$operation,&$messages){
+function validate(&$loan_am,&$show_rate,&$term,&$messages){
 	// sprawdzenie, czy parametry zostały przekazane
-	if ( ! (isset($x) && isset($y) && isset($operation))) {
+	if ( ! (isset($loan_am) && isset($show_rate) && isset($term))) {
 		// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
 		// teraz zakładamy, ze nie jest to błąd. Po prostu nie wykonamy obliczeń
 		return false;
 	}
 
 	// sprawdzenie, czy potrzebne wartości zostały przekazane
-	if ( $x == "") {
-		$messages [] = 'Nie podano liczby 1';
+	if ( $loan_am == "") {
+		$messages [] = 'Nie podano kwoty kredytu';
 	}
-	if ( $y == "") {
-		$messages [] = 'Nie podano liczby 2';
+	if ( $show_rate == "") {
+		$messages [] = 'Nie podano oprocentowania';
+	}
+	if ( $term == "") {
+		$messages [] = 'Nie podano ilości rat';
 	}
 
 	//nie ma sensu walidować dalej gdy brak parametrów
 	if (count ( $messages ) != 0) return false;
 	
 	// sprawdzenie, czy $x i $y są liczbami całkowitymi
-	if (! is_numeric( $x )) {
-		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	if (! is_numeric( $loan_am )) {
+		$messages [] = 'Kwota kredytu nie jest liczbą całkowitą';
 	}
 	
-	if (! is_numeric( $y )) {
-		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	if (! is_numeric( $show_rate )) {
+		$messages [] = 'Oprocentowanie nie jest liczbą całkowitą';
+	}
+	if (! is_numeric( $term )) {
+		$messages [] = 'Ilośś rat nie jest liczbą całkowitą';
 	}	
 
 	if (count ( $messages ) != 0) return false;
 	else return true;
 }
 
-function process(&$x,&$y,&$operation,&$messages,&$result){
+function process(&$loan_am,&$show_rate,&$term,&$messages,&$result){
 	global $role;
-	
-	//konwersja parametrów na int
-	$x = intval($x);
-	$y = intval($y);
-	
-	//wykonanie operacji
-	switch ($operation) {
-		case 'minus' :
-			if ($role == 'admin'){
-				$result = $x - $y;
-			} else {
-				$messages [] = 'Tylko administrator może odejmować !';
-			}
-			break;
-		case 'times' :
-			$result = $x * $y;
-			break;
-		case 'div' :
-			if ($role == 'admin'){
-				$result = $x / $y;
-			} else {
-				$messages [] = 'Tylko administrator może dzielić !';
-			}
-			break;
-		default :
-			$result = $x + $y;
-			break;
+
+	if( $role != 'admin' && $loan_am > 10000) {
+		$messages [] = 'Nie jesteś adminem, więc kwota nie może byc większa, niż 10 000 zł !!!';
+	}else{
+		//konwersja parametrów na int
+		$loan_am = intval($loan_am);
+		$rate = intval($show_rate);
+		$term = intval($term);
+
+		$rate = ($rate/100)/12;
+		
+		//wykonanie operacji
+		$result = ($loan_am * $rate*(1+ $rate)**$term)/((1+ $rate)**$term - 1);
+		$result = $result * 100;
+		$result = intval( $result );
+		$result = $result/100;
 	}
 }
 
 //definicja zmiennych kontrolera
-$x = null;
-$y = null;
-$operation = null;
+$loan_am = null;
+$show_rate = null;
+$term = null;
 $result = null;
 $messages = array();
 
 //pobierz parametry i wykonaj zadanie jeśli wszystko w porządku
-getParams($x,$y,$operation);
-if ( validate($x,$y,$operation,$messages) ) { // gdy brak błędów
-	process($x,$y,$operation,$messages,$result);
+getParams($loan_am,$show_rate,$term);
+if ( validate($loan_am,$show_rate,$term,$messages) ) { // gdy brak błędów
+	process($loan_am,$show_rate,$term,$messages,$result);
 }
 
 // Wywołanie widoku z przekazaniem zmiennych
 // - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
 //   będą dostępne w dołączonym skrypcie
 include 'calc_view.php';
+
+{
+// // 1. pobranie parametrów
+// global $role;
+
+// $loan_am = isset($_REQUEST ['loan_am']) ? $_REQUEST['loan_am'] : null;
+// $show_rate = isset($_REQUEST ['rate']) ? $_REQUEST['rate'] : null;
+// $term = isset($_REQUEST ['term']) ? $_REQUEST['term'] : null;
+
+// if($loan_am !=null){
+
+// // 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// // sprawdzenie, czy parametry zostały przekazane
+// if ( ! (isset($loan_am) && isset($show_rate) && isset($term))) {
+// 	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+// 	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+// }
+
+// // sprawdzenie, czy potrzebne wartości zostały przekazane
+// if ( $loan_am == "") {
+// 	$messages [] = 'Nie podano kwoty kredytu';
+// }
+// if ( $show_rate == "") {
+// 	$messages [] = 'Nie podano oprocentowania';
+// }
+// if ( $term == '') {
+// 	$messages [] = 'Nie podano ilości rat';
+// }
+
+// //nie ma sensu walidować dalej gdy brak parametrów
+// if (empty( $messages )) {
+	
+// 	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+// 	if (! is_numeric( $loan_am )) {
+// 		$messages [] = 'Kwota kredytu nie jest liczbą całkowitą';
+// 	}else{
+// 		if($role != 'admin' && $loan_am > 10000){
+// 			$messages [] = 'Nie jesteś adminem, więc kwota nie może byc większa, niż 10 000 zł !!!';
+// 		}
+// 	}
+	
+// 	if (! is_numeric( $show_rate )) {
+// 		$messages [] = 'Oprocentowanie nie jest liczbą całkowitą';
+// 	}
+	
+// 	if (! is_numeric( $term )) {
+// 		$messages [] = 'Ilośś rat nie jest liczbą całkowitą';
+// 	}
+
+// }
+
+// // 3. wykonaj zadanie jeśli wszystko w porządku
+
+// if (empty ( $messages )) { // gdy brak błędów
+	
+// 	//konwersja parametrów na int
+// 	$loan_am = intval($loan_am);
+// 	$rate = intval($show_rate);
+// 	$term = intval($term);
+
+// 	$rate = ($rate/100)/12;
+	
+// 	//wykonanie operacji
+// 	$result = ($loan_am * $rate*(1+ $rate)**$term)/((1+ $rate)**$term - 1);
+// 	$result = $result * 100;
+// 	$result = intval( $result );
+// 	$result = $result/100;
+// }
+// }
+// // 4. Wywołanie widoku z przekazaniem zmiennych
+// // - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+// //   będą dostępne w dołączonym skrypcie
+// include 'calc_view.php';
+}
--- ochrona-zasobów/app/calc_view.php
+++ ochrona-zasobów/app/calc_view.php
@@ -1,46 +1,65 @@
-<?php
-//Tu już nie ładujemy konfiguracji - sam widok nie będzie już punktem wejścia do aplikacji.
-//Wszystkie żądania idą do kontrolera, a kontroler wywołuje skrypt widoku.
+<?php require_once dirname(__FILE__) .'/../config.php';
 ?>
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
-	<meta charset="utf-8" />
-	<title>Kalkulator</title>
-	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+	<style>
+        .block {
+			width: 50%;
+            display: inline-block; 
+        }
+		.form_block{
+			width: 25%;
+            display: inline-block;
+			
+		}
+		.logout_block{
+			width: 25%;
+            display: grid; 
+		}
+    </style>
 </head>
 <body>
 
-<div style="width:90%; margin: 2em auto;">
-	<a href="<?php print(_APP_ROOT); ?>/app/inna_chroniona.php" class="pure-button">kolejna chroniona strona</a>
-	<a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" class="pure-button pure-button-active">Wyloguj</a>
-</div>
+<form class="logout_block" action="<?php print(_APP_ROOT); ?>/app/security/logout.php">
+		<button type="submit">Wyloguj</button>
+		<br>
+		<br>
+		
+</form>
 
-<div style="width:90%; margin: 2em auto;">
 
-<form action="<?php print(_APP_ROOT); ?>/app/calc.php" method="post" class="pure-form pure-form-stacked">
-	<legend>Kalkulator</legend>
+<form class="form_block" action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<legend>Kalkulator kredytowy</legend>
 	<fieldset>
-		<label for="id_x">Liczba 1: </label>
-		<input id="id_x" type="text" name="x" value="<?php out($x) ?>" />
-		<label for="id_op">Operacja: </label>
-		<select name="op">	
-			<option value="plus">+</option>
-			<option value="minus">-</option>
-			<option value="times">*</option>
-			<option value="div">/</option>
-		</select>
-		<label for="id_y">Liczba 2: </label>
-		<input id="id_y" type="text" name="y" value="<?php out($y) ?>" />
+		<label class="block" for="loan-amount">Kwota kredytu: </label>
+		<input id="loan-amount" type="text" name="loan_am" value="<?php isset($loan_am)? print($loan_am): "Write number"; ?>">
+		<br>
+
+		<br>
+		<label class="block" for="credit-rate">Oprocentowanie (%): </label>
+		<input id="credit-rate" type="text" name="rate" value="<?php isset($show_rate)? print($show_rate): "Write number"; ?>" />
+		<br />
+
+		<br>
+		<label class="block" for="loan-term">Ilość rat miesięcznych: </label>
+		<input id="loan-term" type="text" name="term" value="<?php isset($term)? print($term): "Write number"; ?>" />
+		<br />
+		
+		<br>
+		<input type="submit" value="Oblicz" />
+		<br/>
+		</table>
 	</fieldset>	
-	<input type="submit" value="Oblicz" class="pure-button pure-button-primary" />
 </form>	
 
 <?php
 //wyświeltenie listy błędów, jeśli istnieją
 if (isset($messages)) {
 	if (count ( $messages ) > 0) {
-		echo '<ol style="margin-top: 1em; padding: 1em 1em 1em 2em; border-radius: 0.5em; background-color: #f88; width:25em;">';
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
 		foreach ( $messages as $key => $msg ) {
 			echo '<li>'.$msg.'</li>';
 		}
@@ -50,12 +69,10 @@ if (isset($messages)) {
 ?>
 
 <?php if (isset($result)){ ?>
-<div style="margin-top: 1em; padding: 1em; border-radius: 0.5em; background-color: #ff0; width:25em;">
-<?php echo 'Wynik: '.$result; ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Rata miesięczna: '.$result.' zł'?>
 </div>
 <?php } ?>
-
-</div>
 
 </body>
 </html>
